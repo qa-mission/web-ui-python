f8b853f6fb1b22fb26a8e9cf35d6a41a1fd7f205
gpawel <gpawel@gmail.com>
2024-02-12 18:01:43 -0500

    added query value.

--- tests/assignment1.py
+++ tests/assignment1.py
@@ -5,6 +5,7 @@ from models.shopping_cart import ShoppingCart
 
 def test_add_item_to_shopping_cart():
     # Assuming loblawsPage is already initialized and available here
+    query = 'apple'
     resultsPage = loblawsPage.search(query)
     productInCart = resultsPage.get_product_info_by_index(1)
     cart = resultsPage.add_item_to_shopping_cart(1)
