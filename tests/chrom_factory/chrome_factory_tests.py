210c4103eb930489dc4d45727901ba8366776f75
gpawel <gpawel@gmail.com>
2024-02-17 19:39:57 -0500

    added chrome driver start, go to a page and check the page is loaded test.

--- tests/chrom_factory/chrome_factory_tests.py
+++ tests/chrom_factory/chrome_factory_tests.py
@@ -1,5 +1,9 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from config.config import Config
 from driver.chrome_driver_factory import ChromeDriverFactory
 
 
@@ -25,8 +29,26 @@ class TestChromeDriverFactory:
 
         # Assert
         assert chrome_options.page_load_strategy == expected_page_load_strategy
-        assert chrome_options.accept_insecure_certs != expected_disable_extensions  # This might need to be revised based on actual option being tested
+        assert chrome_options.accept_insecure_certs == expected_disable_extensions
         assert chrome_options.experimental_options.get('useAutomationExtension') == expected_use_automation_extension
+
+    def test_start_chrome_driver(self):
+        # Assert driver is not None
+        assert TestChromeDriverFactory.driver is not None
+        config_instance = Config("linux")
+        # Navigate to the URL
+        TestChromeDriverFactory.driver.get("https://www.loblaws.ca/")
+        TestChromeDriverFactory.driver.maximize_window()
+
+        # Wait for elements to be present
+        wait = WebDriverWait(TestChromeDriverFactory.driver, config_instance.get_pause_in_test())
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".logo")))
+        wait.until(EC.presence_of_element_located((By.ID, "site-content")))
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.booking-selector")))
+
+        # Assert text of a specific element
+        element = TestChromeDriverFactory.driver.find_element(By.CSS_SELECTOR, "div.booking-selector")
+        assert element.text == "Glen Erin Drive".upper()
 
     @classmethod
     def test_create_chrome_driver(cls):
